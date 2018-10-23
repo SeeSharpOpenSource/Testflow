068f4017ac1a91642b3ec7fd4bf8511ad8819229
= <=>
2018-10-23 17:02:20 +0800

    提交i18n工程的创建

---
+++ src/Common/i18n/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Testflow.i18n
+{
+    public static class Constants
+    {
+        public const int DefaultEntityCount = 8;
+    }
+}
---
+++ src/Common/i18n/I18n.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Testflow.DataInterface;
+
+namespace Testflow.i18n
+{
+    public class I18n
+    {
+        private static ConcurrentDictionary<long, I18n> _i18nEntities = new ConcurrentDictionary<long, I18n>();
+        private IRuntimeContext context;
+
+        public I18n(IRuntimeContext context)
+        {
+            this.context = context;
+            // TODO to be implemented
+        }
+
+        public static I18n GetInstance(IRuntimeContext context)
+        {
+            long sessionId = context.SessionId;
+            if (!_i18nEntities.ContainsKey(sessionId))
+            {
+                _i18nEntities.TryAdd(sessionId, new I18n(context));
+            }
+            return _i18nEntities[context.SessionId];
+        }
+    }
+}
