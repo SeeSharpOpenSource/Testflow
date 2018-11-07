54777ca741d47f3bf4aae0f02a363554e6763d41
= <=>
2018-11-07 18:07:37 +0800

    新增i18n的接口和IEntityComponent接口

---
+++ source/src/Dev/Common/Common/I18nInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testflow.Common
+{
+    public interface I18NInterface
+    {
+        string GetStr(string labelKey);
+        string GetFStr(string labelKey, params object[] param);
+    }
+}
---
+++ source/src/Dev/Common/Common/IEntityComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testflow.Common
+{
+    public interface IEntityComponent : IDisposable
+    {
+        /// <summary>
+        /// 初始化组件
+        /// </summary>
+        void Initialize();
+    }
+}
