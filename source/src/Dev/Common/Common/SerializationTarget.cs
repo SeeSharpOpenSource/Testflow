0bacb2ed0793a5fe1ddbb235ad4ff9011fbf2288
= <=>
2018-11-12 17:48:16 +0800

    新增定义SerializationTarget

--- source/src/Dev/Common/Common/SerializationTarget.cs
+++ source/src/Dev/Common/Common/SerializationTarget.cs
@@ -5,6 +5,14 @@
     /// </summary>
     public enum SerializationTarget
     {
-        
+        /// <summary>
+        /// 序列化到文件
+        /// </summary>
+        File,
+
+        /// <summary>
+        /// 序列化到数据库
+        /// </summary>
+        DataBase
     }
 }
