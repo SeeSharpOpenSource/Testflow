c30bafb4c4042cb31cf5be5d1436633677319627
= <=>
2018-11-14 16:39:46 +0800

    简单声明LogService接口，后续持续更新

---
+++ source/src/Dev/Common/Common/LogLevel.cs
@@ -0,0 +1,38 @@
+﻿namespace Testflow.Common
+{
+    /// <summary>
+    /// 日志级别
+    /// </summary>
+    public enum LogLevel
+    {
+        /// <summary>
+        /// 最低级别
+        /// </summary>
+        All = 0,
+
+        /// <summary>
+        /// 调试级别
+        /// </summary>
+        Debug = 1,
+
+        /// <summary>
+        /// 消息级别
+        /// </summary>
+        Info = 2,
+
+        /// <summary>
+        /// 警告级别
+        /// </summary>
+        Warn =3,
+
+        /// <summary>
+        /// 错误级别
+        /// </summary>
+        Error = 4,
+
+        /// <summary>
+        /// 严重错误级别
+        /// </summary>
+        Fatal = 5
+    }
+}
--- source/src/Dev/Common/Modules/ILogService.cs
+++ source/src/Dev/Common/Modules/ILogService.cs
@@ -1,7 +1,35 @@
-﻿namespace Testflow.Modules
+﻿using System;
+using Testflow.Common;
+using Testflow.Runtime;
+
+namespace Testflow.Modules
 {
+    /// <summary>
+    /// 日志服务模块
+    /// </summary>
     public interface ILogService : IController
     {
-         
+        /// <summary>
+        /// 当前的日志级别
+        /// </summary>
+        LogLevel RecordLevel { get; set; }
+
+        /// <summary>
+        /// 以指定级别打印日志
+        /// </summary>
+        /// <param name="logLevel">日志级别</param>
+        /// <param name="context">运行时上下文</param>
+        /// <param name="sequenceIndex">序列索引</param>
+        /// <param name="message">信息</param>
+        void Print(LogLevel logLevel, IRuntimeContext context, int sequenceIndex, string message);
+
+        /// <summary>
+        /// 以指定级别打印异常信息
+        /// </summary>
+        /// <param name="logLevel">日志级别</param>
+        /// <param name="context">运行时上下文</param>
+        /// <param name="sequenceIndex">序列索引</param>
+        /// <param name="exception">待打印的异常</param>
+        void Print(LogLevel logLevel, IRuntimeContext context, int sequenceIndex, Exception exception);
     }
 }
