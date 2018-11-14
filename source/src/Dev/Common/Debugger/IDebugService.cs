8ea2505a20b8495dbb3b61f35f878e40b9b342fd
= <=>
2018-11-14 16:51:03 +0800

    新增Debugger的声明

---
+++ source/src/Dev/Common/Debugger/IDebugService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Testflow.Data.Sequence;
+
+namespace Testflow.Debug
+{
+    /// <summary>
+    /// 调试服务
+    /// </summary>
+    public interface IDebugService
+    {
+        IDebugContext Context { get; }
+
+        
+
+
+    }
+}
---
+++ source/src/Dev/Common/Debugger/IDebugSession.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Testflow.Data.Sequence;
+
+namespace Testflow.Debug
+{
+    public interface IDebugSession
+    {
+        IDebugContext Context { get; }
+
+        IList<ISequenceStep> UnreachedBreakPoints { get; }
+        
+        ISequenceStep CurrentStep { get; }
+
+        
+
+
+    }
+}
