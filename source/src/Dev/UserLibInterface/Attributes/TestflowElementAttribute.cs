3f2112f4b5e18acf1e206668c884b9b29d4910be
= <=>
2018-11-16 17:52:17 +0800

    新增TestflowElement属性，修改TestflowClassAttribute属性

---
+++ source/src/Dev/UserLibInterface/Attributes/TestflowElementAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Testflow.Attributes
+{
+    /// <summary>
+    /// 在Testflow中被使用的元素注解
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class |　AttributeTargets.Method | AttributeTargets.Property)]
+    public class TestflowElementAttribute : Attribute
+    {
+        /// <summary>
+        /// 元素是否为Testflow的可用项
+        /// </summary>
+        public bool IsTestflowElement { get; }
+
+        /// <summary>
+        /// 标记某个类/方法/属性会作为Testflow的使用项目
+        /// </summary>
+        public TestflowElementAttribute()
+        {
+            this.IsTestflowElement = true;
+        }
+
+        /// <summary>
+        /// 是否标记某个类/方法/属性会作为Testflow的使用项目
+        /// </summary>
+        /// <param name="isTestflowElement">是否标记目标为Testflow的可用项目</param>
+        public TestflowElementAttribute(bool isTestflowElement)
+        {
+            this.IsTestflowElement = isTestflowElement;
+        }
+    }
+}
---
+++ source/src/Dev/UserLibInterface/Attributes/TestflowTypeAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Testflow.Attributes
+{
+    /// <summary>
+    /// Testflow中会被使用的数据类型
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TestflowTypeAttribute : Attribute
+    {
+        /// <summary>
+        /// 该类是否是Testflow中会被使用的数据类型
+        /// </summary>
+        public bool IsTestflowDataType { get; }
+
+        /// <summary>
+        /// 标记该类为Testflow中会被使用的数据类型
+        /// </summary>
+        public TestflowTypeAttribute()
+        {
+            this.IsTestflowDataType = true;
+        }
+
+        /// <summary>
+        /// 是否标记该类为Testflow中会被使用的数据类型
+        /// </summary>
+        /// <param name="isDataType">该类是否为Testflow中会被使用的数据类型</param>
+        public TestflowTypeAttribute(bool isDataType)
+        {
+            this.IsTestflowDataType = isDataType;
+        }
+    }
+}
