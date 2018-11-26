829fff2d0f15ea63569001a5d937a090654d6328
= <=>
2018-11-26 14:15:24 +0800

    ISerializableMap增加对xml接口的声明

--- source/src/Dev/Common/Common/ISerializableMap.cs
+++ source/src/Dev/Common/Common/ISerializableMap.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
+using System.Xml.Serialization;
 
 namespace Testflow.Common
 {
@@ -8,7 +9,7 @@ namespace Testflow.Common
     /// </summary>
     /// <typeparam name="TKeyType">键类型</typeparam>
     /// <typeparam name="TValueType">数值类型</typeparam>
-    public interface ISerializableMap<TKeyType, TValueType> : IDictionary<TKeyType, TValueType>, ISerializable
+    public interface ISerializableMap<TKeyType, TValueType> : IDictionary<TKeyType, TValueType>, ISerializable, IXmlSerializable
     {
          
     }
