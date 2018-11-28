928e50b999cb41e54f7539361478252e404f3137
= <=>
2018-11-28 15:34:27 +0800

    修改ITypeDataCollection的类型，从IList到ISerializableMap

--- source/src/Dev/Common/Data/Sequence/ITypeDataCollection.cs
+++ source/src/Dev/Common/Data/Sequence/ITypeDataCollection.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
+using Testflow.Common;
 
 namespace Testflow.Data.Sequence
 {
     /// <summary>
     /// 测试序列组使用的所有Type的集合
     /// </summary>
-    public interface ITypeDataCollection : IList<ITypeData>
+    public interface ITypeDataCollection : ISerializableMap<int, ITypeData>
     {
          
     }
