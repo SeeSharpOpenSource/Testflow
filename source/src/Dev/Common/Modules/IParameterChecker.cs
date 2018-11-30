13dddab439f2587e078f3651a62d605ae17862a4
= <=>
2018-11-30 16:45:44 +0800

    ParameterChecker中新增校验Sequence参数配置的接口

--- source/src/Dev/Common/Modules/IParameterChecker.cs
+++ source/src/Dev/Common/Modules/IParameterChecker.cs
@@ -21,8 +21,17 @@ namespace Testflow.Modules
         /// 校验SequenceGroup内模块的参数配置正确性
         /// </summary>
         /// <param name="components">设计时加载的组件映射集合</param>
-        /// <param name="testProject">待校验的序列组</param>
+        /// <param name="sequenceGroup">待校验的序列组</param>
         /// <returns>检查过程中出现的告警信息</returns>
-        IList<IWarningInfo> CheckParameters(IDictionary<string, IComInterfaceDescription> components, ISequenceGroup testProject);
+        IList<IWarningInfo> CheckParameters(IDictionary<string, IComInterfaceDescription> components, ISequenceGroup sequenceGroup);
+
+        /// <summary>
+        /// 校验Sequence模块的参数配置正确性
+        /// </summary>
+        /// <param name="components">设计时加载的组件映射集合</param>
+        /// <param name="sequenceGroup">待校验的序列组</param>
+        /// <param name="sequence">待校验的序列</param>
+        /// <returns>检查过程中出现的告警信息</returns>
+        IList<IWarningInfo> CheckParameters(IDictionary<string, IComInterfaceDescription> components, ISequenceGroup sequenceGroup, ISequence sequence);
     }
 }
