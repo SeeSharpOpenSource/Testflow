4ac06a6cf5c8fd1c42e2f1b6f93bc196d1c88341
= <=>
2018-12-14 17:44:08 +0800

    新增I18nOption类，TestflowRunner新增GetI18n接口以获取对应的资源文件

---
+++ source/src/Dev/Common/Common/I18nOption.cs
@@ -0,0 +1,42 @@
+﻿namespace Testflow.Common
+{
+    /// <summary>
+    /// I18n参数配置类
+    /// </summary>
+    public class I18nOption
+    {
+        /// <summary>
+        /// 创建I18n参数类
+        /// </summary>
+        /// <param name="firstLanFile">第一语言信息所在资源文件</param>
+        /// <param name="secondLanFile">第二语言信息所在资源文件</param>
+        public I18nOption(string firstLanFile, string secondLanFile)
+        {
+            this.FirstLanguage = "zh-CN";
+            this.SecondLanguage = "en-US";
+
+            this.FirstLanFile = firstLanFile;
+            this.SecondLanFile = secondLanFile;
+        }
+
+        /// <summary>
+        /// 第一语言所在资源文件
+        /// </summary>
+        public string FirstLanFile { get; set; }
+        
+        /// <summary>
+        /// 第二语言所在资源文件
+        /// </summary>
+        public string SecondLanFile { get; set; }
+        
+        /// <summary>
+        /// 第一语言名称
+        /// </summary>
+        public string FirstLanguage { get; set; }
+        
+        /// <summary>
+        /// 第二语言名称
+        /// </summary>
+        public string SecondLanguage { get; set; }
+    }
+}
--- source/src/Dev/Common/TestFlowRunner.cs
+++ source/src/Dev/Common/TestFlowRunner.cs
@@ -126,9 +126,9 @@ namespace Testflow
         #region 辅助组件接口
 
         /// <summary>
-        /// 国际化组件
+        /// 获取对应配置的国际化组件
         /// </summary>
-        public abstract I18NInterface I18n { get; }
+        public abstract I18NInterface GetI18n(I18nOption option);
 
         #endregion
 
