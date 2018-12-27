00c9683a5067012f81cbbfe550d4001d959ac812
= <=>
2018-12-27 17:34:37 +0800

    IParameterChecker新增校验属性类型的接口

--- source/src/Dev/Common/Modules/IParameterChecker.cs
+++ source/src/Dev/Common/Modules/IParameterChecker.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Testflow.Data;
 using Testflow.Data.Description;
 using Testflow.Data.Sequence;
 
@@ -12,26 +13,32 @@ namespace Testflow.Modules
         /// <summary>
         /// 校验TestProject内模块的参数配置正确性
         /// </summary>
-        /// <param name="components">设计时加载的组件映射集合</param>
         /// <param name="testProject">待校验的序列工程</param>
         /// <returns>检查过程中出现的告警信息</returns>
-        IList<IWarningInfo> CheckParameters(IDictionary<string, IComInterfaceDescription> components, ITestProject testProject);
+        IList<IWarningInfo> CheckParameters(ITestProject testProject);
 
         /// <summary>
         /// 校验SequenceGroup内模块的参数配置正确性
         /// </summary>
-        /// <param name="components">设计时加载的组件映射集合</param>
         /// <param name="sequenceGroup">待校验的序列组</param>
         /// <returns>检查过程中出现的告警信息</returns>
-        IList<IWarningInfo> CheckParameters(IDictionary<string, IComInterfaceDescription> components, ISequenceGroup sequenceGroup);
+        IList<IWarningInfo> CheckParameters(ISequenceGroup sequenceGroup);
 
         /// <summary>
         /// 校验Sequence模块的参数配置正确性
         /// </summary>
-        /// <param name="components">设计时加载的组件映射集合</param>
         /// <param name="sequenceGroup">待校验的序列组</param>
         /// <param name="sequence">待校验的序列</param>
         /// <returns>检查过程中出现的告警信息</returns>
-        IList<IWarningInfo> CheckParameters(IDictionary<string, IComInterfaceDescription> components, ISequenceGroup sequenceGroup, ISequence sequence);
+        IList<IWarningInfo> CheckParameters(ISequenceGroup sequenceGroup, ISequence sequence);
+
+        /// <summary>
+        /// 校验某个变量和属性构成的字符串对应的类型是否和待校验类型一致
+        /// </summary>
+        /// <param name="parent">该次所在的SequenceGroup或TestProject</param>
+        /// <param name="variableString">变量的字符串，样式类似于varname.property1.property2</param>
+        /// <param name="checkType">待检查是否匹配的类型</param>
+        /// <returns>如果错误返回错误信息，如果正确返回true</returns>
+        IWarningInfo CheckPropertyType(ISequenceFlowContainer parent, string variableString, ITypeData checkType);
     }
 }
