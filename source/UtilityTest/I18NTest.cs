4094f1b72ab8055945e7996e146b92899641b9e9
= <=>
2019-02-01 16:58:13 +0800

    提交Utility的单元测试

---
+++ source/UtilityTest/I18NTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testflow.Dev.UtilityTest.Resources;
+using Testflow.Utility.I18nUtil;
+
+namespace Testflow.Dev.UtilityTest
+{
+    [TestClass]
+    public class I18NTest
+    {
+        private I18N _i18N;
+        private I18NOption _i18NOption;
+        const string I18nName = "i18nTest";
+        const string TestLabel = "TestLabel";
+        [TestInitialize]
+        public void SetUp()
+        {
+            _i18NOption = new I18NOption(typeof (I18NTest).Assembly, "i18n_test_cn",
+                "i18n_test_en")
+            {
+                Name = I18nName
+            };
+            _i18N = I18N.GetInstance(_i18NOption);
+        }
+
+        [TestMethod]
+        public void GetEntity()
+        {
+            I18N i18N = I18N.GetInstance(I18nName);
+            Assert.AreNotEqual(null, i18N);
+        }
+
+        [TestMethod]
+        public void I18NChineseTest()
+        {
+            I18N.RemoveInstance(I18nName);
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("zh-CN");
+            _i18N = I18N.GetInstance(_i18NOption);
+            string str = _i18N.GetStr(TestLabel);
+            Assert.AreEqual(str, i18n_test_cn.TestLabel);
+
+        }
+
+        [TestMethod]
+        public void I18NEnglishTest()
+        {
+            I18N.RemoveInstance(I18nName);
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            _i18N = I18N.GetInstance(_i18NOption);
+            string str = _i18N.GetStr(TestLabel);
+            Assert.AreEqual(str, i18n_test_en.TestLabel);
+        }
+    }
+}
---
+++ source/UtilityTest/MessengeTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testflow.Utility.MessageUtil;
+
+namespace Testflow.Dev.UtilityTest
+{
+    [TestClass]
+    public class MessengeTest
+    {
+        private Messenger _messenger;
+        private MessengerOption _option;
+        const string MsgQueueName = "Test";
+        const int MaxSession = 10;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _option = new MessengerOption(MsgQueueName, new Type[] {typeof (TestMessage)});
+            _messenger = Messenger.CreateMessenger(_option);
+            IMessageConsumer[] consumers = new IMessageConsumer[MaxSession];
+            for (int i = 0; i < MaxSession; i++)
+            {
+                consumers[i] = new TestConsumer(i);
+            }
+            _messenger.Initialize(consumers);
+        }
+
+        [TestMethod]
+        public void GetMessage()
+        {
+            Messenger messenger = Messenger.GetMessenger(_option);
+            Assert.IsNotNull(messenger);
+        }
+
+        [TestMethod]
+        public void TestConsumer()
+        {
+            for (int i = 0; i < MaxSession; i++)
+            {
+                TestMessage message = new TestMessage()
+                {
+                    Id = i,
+                    Message = CreateTestMessage(i)
+                };
+                _messenger.Send(message, FormatterType.Xml, null);
+                Thread.Sleep(100);
+                Assert.AreEqual(_messenger.MessageCount, 0);
+            }
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _messenger?.Dispose();
+        }
+
+
+        public static string CreateTestMessage(int id)
+        {
+            return $"Message to session {id}.";
+        }
+    }
+
+    public class TestMessage : IMessage
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class TestConsumer : IMessageConsumer
+    {
+        public TestConsumer(int id)
+        {
+            this.SessionId = id;
+        }
+        public int SessionId { get; }
+        public void Handle(IMessage message)
+        {
+            string expect = MessengeTest.CreateTestMessage(SessionId);
+            Assert.AreEqual(expect, message);
+        }
+    }
+}
