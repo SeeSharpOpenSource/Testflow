3e1b2ba77fd8a49f98e7953a0a27749c99acbc08
= <=>
2019-02-13 10:42:14 +0800

    SequenceContainer接口新增克隆接口方法

--- source/src/Dev/Common/Data/Sequence/ISequenceDataContainer.cs
+++ source/src/Dev/Common/Data/Sequence/ISequenceDataContainer.cs
@@ -1,10 +1,15 @@
-﻿namespace Testflow.Data.Sequence
+﻿using System;
+
+namespace Testflow.Data.Sequence
 {
     /// <summary>
     /// 序列容器容器的父接口
     /// </summary>
     public interface ISequenceDataContainer : ISequenceElement
     {
-         
+        /// <summary>
+        /// 克隆一个复制数据
+        /// </summary>
+        ISequenceDataContainer Clone();
     }
 }
