0b5052aae3794505c9c8662db166f09ed72ebc66
= <=>
2019-02-22 18:18:42 +0800

    提交XmlReaderHelper的测试代码

--- source/test/Modules/SequenceManagerTest/SequenceDeSerializeTest.cs
+++ source/test/Modules/SequenceManagerTest/SequenceDeSerializeTest.cs
@@ -9,6 +9,7 @@ using Testflow.Data.Description;
 using Testflow.Data.Sequence;
 using Testflow.Modules;
 using Testflow.SequenceManager.SequenceElements;
+using Testflow.SequenceManager.Serializer;
 using Testflow.Utility.Collections;
 
 namespace Testflow.SequenceManagerTest
@@ -66,101 +67,13 @@ namespace Testflow.SequenceManagerTest
         #endregion
 
         [TestMethod]
-        public void TestProjectSerialize()
+        public void TestXmlReader()
         {
-            ITestProject testProject = _sequenceManager.CreateTestProject();
-
-            testProject.Initialize(null);
-
-            Assert.AreEqual(testProject.ModelVersion, _configData.GetProperty<string>("ModelVersion"));
-            ISequenceGroup sequenceGroup1 = _sequenceManager.CreateSequenceGroup();
-            ISequenceGroup sequenceGroup2 = _sequenceManager.CreateSequenceGroup();
-            ISequenceGroup sequenceGroup3 = _sequenceManager.CreateSequenceGroup();
-            sequenceGroup1.Initialize(testProject);
-            testProject.SequenceGroups.Add(sequenceGroup1);
-
-
-            sequenceGroup2.Initialize(testProject);
-            testProject.SequenceGroups.Add(sequenceGroup2);
-
-            sequenceGroup3.Initialize(testProject);
-            testProject.SequenceGroups.Add(sequenceGroup3);
-
-            ISequence sequence1 = _sequenceManager.CreateSequence();
-            ISequence sequence2 = _sequenceManager.CreateSequence();
-            ISequence sequence3 = _sequenceManager.CreateSequence();
-            sequence1.Initialize(sequenceGroup1);
-            sequenceGroup1.Sequences.Add(sequence1);
-
-            sequence2.Initialize(sequenceGroup1);
-            sequenceGroup1.Sequences.Add(sequence2);
-
-            sequence3.Initialize(sequenceGroup1);
-            sequenceGroup1.Sequences.Add(sequence3);
-
-            Assert.AreEqual(sequence1.Index, 0);
-            Assert.AreEqual(sequence2.Index, 1);
-            Assert.AreEqual(sequence3.Index, 2);
-
-            ISequenceStep sequenceStep1 = _sequenceManager.CreateSequenceStep();
-            ISequenceStep sequenceStep2 = _sequenceManager.CreateSequenceStep();
-            ISequenceStep sequenceStep3 = _sequenceManager.CreateSequenceStep();
-
-            sequenceStep1.Initialize(sequence1);
-            sequence1.Steps.Add(sequenceStep1);
-
-            sequenceStep2.Initialize(sequence1);
-            sequence1.Steps.Add(sequenceStep2);
-
-            sequenceStep3.Initialize(sequence1);
-            sequence1.Steps.Add(sequenceStep3);
-
-            Assert.AreEqual(sequenceStep1.Index, 0);
-            Assert.AreEqual(sequenceStep2.Index, 1);
-            Assert.AreEqual(sequenceStep3.Index, 2);
-
-            IVariable varialbe1 = _sequenceManager.CreateVarialbe();
-            varialbe1.Initialize(sequence1);
-            sequence1.Variables.Add(varialbe1);
-            varialbe1.Value = "10";
-
-            IVariable varialbe2 = _sequenceManager.CreateVarialbe();
-            varialbe2.Initialize(sequence1);
-            sequence1.Variables.Add(varialbe2);
-            varialbe2.Value = "20";
-
-            IVariable varialbe3 = _sequenceManager.CreateVarialbe();
-            varialbe3.Initialize(sequence1);
-            sequence1.Variables.Add(varialbe3);
-            varialbe3.Value = "30";
-
-            IFunctionData functionData = _sequenceManager.CreateFunctionData(new TestFuncDescription());
-            sequenceStep1.Function = functionData;
-            sequenceStep3.Function = functionData;
-            functionData.Instance = "Variable1";
-            functionData.Return = "Variable3";
-
-            sequenceStep1.RetryCounter = new RetryCounter()
-            {
-                CounterVariable = "varialbe1",
-                MaxRetryTimes = 10,
-                Name = "RetryCounterDemo",
-                RetryEnabled = true
-            };
-
-            sequenceStep3.LoopCounter = new LoopCounter()
-            {
-                CounterVariable = "Variable2",
-                CounterEnabled = true,
-                MaxValue = 20,
-                Name = "LoopCounterDemo"
-            };
-
-            _sequenceManager.Serialize(testProject, SerializationTarget.File, @"D:\testflow\test.tfproj");
+            TestProject testProject = XmlReaderHelper.ReadTestProject(@"D:\testflow\test.tfproj");
         }
 
         [TestMethod]
-        public void SequenceGroupSerialize()
+        public void TestProjectDeSerialize()
         {
             ISequenceGroup sequenceGroup1 = _sequenceManager.CreateSequenceGroup();
             sequenceGroup1.Initialize(null);
