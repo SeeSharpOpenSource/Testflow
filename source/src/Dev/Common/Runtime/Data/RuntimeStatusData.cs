799c216381fbb3e42aaa5f7472f54f58d6e8df9e
tao <tao.jing@jytek.com>
2019-04-02 11:07:27 +0800

    RuntimeStatusData新增StatusIndex值

--- source/src/Dev/Common/Runtime/Data/RuntimeStatusData.cs
+++ source/src/Dev/Common/Runtime/Data/RuntimeStatusData.cs
@@ -23,6 +23,11 @@ namespace Testflow.Runtime.Data
         public int Sequence { get; set; }
 
         /// <summary>
+        /// 状态索引
+        /// </summary>
+        public int StatusIndex { get; set; }
+
+        /// <summary>
         /// 当前时间
         /// </summary>
         public DateTime Time { get; set; }
