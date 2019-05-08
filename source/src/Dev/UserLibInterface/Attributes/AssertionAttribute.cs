b0137e1aa7793e3165d6c1e92a827dac73e0578e
tao <tao.jing@jytek.com>
2019-05-08 15:47:45 +0800

    UserLib工程新增AssertionAttribute

---
+++ source/src/Dev/UserLibInterface/Attributes/AssertionAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testflow.Usr.Attributes
+{
+    /// <summary>
+    /// 用于断言声明的注解
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AssertionAttribute : Attribute
+    {
+    }
+}
