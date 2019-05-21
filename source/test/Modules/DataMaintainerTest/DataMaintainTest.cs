39ec37fdd169cb6bbc52db5336ae1914855d3ad4
tao <tao.jing@jytek.com>
2019-05-21 10:57:30 +0800

    修改所有Test工程输出到父目录的testexport目录

---
+++ source/test/Modules/DataMaintainerTest/DataMaintainTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Common;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testflow.DataMaintainerTest
+{
+    [TestClass]
+    public class DataMaintainTest
+    {
+        private DbConnection _dbConnection;
+
+        public DataMaintainTest()
+        {
+            const string dataBaseName = "testflowData.db3";
+            if (File.Exists(dataBaseName))
+            {
+                File.Delete(dataBaseName);
+            }
+            
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            DbProviderFactory factory = DbProviderFactories.GetFactory("System.Data.SQLite");
+            _dbConnection = factory.CreateConnection();
+            _dbConnection.ConnectionString = "Data Source=test.db3";
+            _dbConnection.Open();
+        }
+    }
+}
