ebd573413ec21d90c51152bf88cc7b9ced85fcd3
tao <tao.jing@jytek.com>
2019-05-30 11:46:51 +0800

    删除Common库中无用的日志类

--- source/src/Dev/Common/Modules/ILogService.cs
+++ source/src/Dev/Common/Modules/ILogService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Testflow.Usr;
-using Testflow.Log;
 using Testflow.Runtime;
 
 namespace Testflow.Modules
@@ -21,18 +20,18 @@ namespace Testflow.Modules
         /// 以指定级别打印日志
         /// </summary>
         /// <param name="logLevel">日志级别</param>
-        /// <param name="sequenceIndex">序列索引</param>
+        /// <param name="sessionId">序列索引</param>
         /// <param name="message">信息</param>
-        void Print(LogLevel logLevel, int sequenceIndex, string message);
+        void Print(LogLevel logLevel, int sessionId, string message);
 
         /// <summary>
         /// 以指定级别打印异常信息
         /// </summary>
         /// <param name="logLevel">日志级别</param>
-        /// <param name="sequenceIndex">序列索引</param>
+        /// <param name="sessionId">序列索引</param>
         /// <param name="exception">待打印的异常</param>
         /// <param name="message">日志信息</param>
-        void Print(LogLevel logLevel, int sequenceIndex, Exception exception, string message = "");
+        void Print(LogLevel logLevel, int sessionId, Exception exception, string message = "");
 
         #endregion
 
@@ -40,12 +39,6 @@ namespace Testflow.Modules
         /// 运行时的日志级别
         /// </summary>
         LogLevel RuntimeLogLevel { get; set; }
-
-        /// <summary>
-        /// 创建某个运行时会话的日志流
-        /// </summary>
-        /// <param name="sessionId">运行时会话ID。-1为系统自留的日志，该日志流不能被手动销毁</param>
-        ILogSession GetLogSession(int sessionId);
 
         /// <summary>
         /// 在框架中以指定级别向某个运行时会话的日志流打印日志
@@ -65,16 +58,5 @@ namespace Testflow.Modules
         /// <param name="exception">待打印的异常</param>
         /// <param name="message"></param>
         void Print(LogLevel logLevel, int sessionId, int sequenceIndex, Exception exception, string message = "");
-
-        /// <summary>
-        /// 删除运行时ID对应的日志流
-        /// </summary>
-        /// <param name="sessionId">日志会话ID。-1为系统自留的日志，该日志流不能被手动销毁</param>
-        void DestroyLogStream(int sessionId);
-
-        /// <summary>
-        /// 删除所有运行时日志流
-        /// </summary>
-        void DestroyLogStream();
     }
 }
