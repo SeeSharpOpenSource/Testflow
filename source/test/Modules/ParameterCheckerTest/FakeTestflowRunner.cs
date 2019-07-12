5ce897d041d0f458a1d8336903c72d7047b3a185
catsae <stanlegend@163.com>
2019-07-12 16:47:20 +0800

    正在谱写单元测试， 一小步一小步来

---
+++ source/test/Modules/ParameterCheckerTest/FakeTestflowRunner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testflow.Data;
+using Testflow.Data.Description;
+using Testflow.Modules;
+
+namespace Testflow.ParameterCheckerTest
+{
+    class FakeTestflowRunner:TestflowRunner
+    {
+        public FakeTestflowRunner(TestflowRunnerOptions options) : base(options)
+        {
+        }
+
+        #region 模块属性
+        public override IComInterfaceManager ComInterfaceManager { get; protected set; }
+        public override IConfigurationManager ConfigurationManager { get; protected set; }
+        public override IDataMaintainer DataMaintainer { get; protected set; }
+        public override IEngineController EngineController { get; protected set; }
+        public override ILogService LogService { get; protected set; }
+        public override IParameterChecker ParameterChecker { get; protected set; }
+        public override IResultManager ResultManager { get; protected set; }
+        public override ISequenceManager SequenceManager { get; protected set; }
+        #endregion
+
+        public override void Initialize()
+        {
+            this.SequenceManager = new SequenceManager.SequenceManager();
+
+            this.ComInterfaceManager = new ComInterfaceManager.InterfaceManager();
+
+            this.ParameterChecker = new ParameterChecker.ParameterChecker();
+
+            this.LogService = new Logger.LogService();
+
+            SequenceManager.DesigntimeInitialize();
+            LogService.RuntimeInitialize();
+        }
+
+        public override void Dispose()
+        {
+        }
+
+        //假的组件描述信息类
+        public class FakeComInterface : IComInterfaceDescription
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public int ComponentId { get; set; }
+            public string Signature { get; }
+            public IAssemblyInfo Assembly { get; set; }
+            public IList<IClassInterfaceDescription> Classes { get; }
+            public IList<ITypeData> VariableTypes { get; set; }
+            public string Category { get; set; }
+            public IDictionary<string, string[]> Enumerations { get; set; }
+        }
+    }
+}
---
+++ source/test/Modules/ParameterCheckerTest/ParameterCheckTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testflow.Modules;
+using System.Reflection;
+using Testflow.Data.Description;
+using Testflow.SequenceManager.SequenceElements;
+using Testflow.Data.Sequence;
+
+namespace Testflow.ParameterCheckerTest
+{
+    [TestClass]
+    public class ParameterCheckerTest
+    {
+        private ISequenceManager _sequenceManager;
+        private IParameterChecker _parameterChecker;
+        private IComInterfaceManager _interfaceManager;
+        private const string path = @"C:\SeeSharp\JYTEK\Hardware\DAQ\JYUSB61902\Bin\";
+        private const string testProjectPath = path + "ParameterCheckerTest.tfseq";
+
+        public ParameterCheckerTest()
+        {
+            #region 创建并初始化假的TestFlowRunner
+            Type runnerType = typeof(TestflowRunner);
+            //默认options
+            TestflowRunnerOptions option = new TestflowRunnerOptions();
+            //创建假的TestFlowRunner
+            FakeTestflowRunner fakeTestflowRunner = new FakeTestflowRunner(option);
+            Type intType = typeof(int);
+            //用反射将获取private fieldInfo，然后赋值fake
+            FieldInfo fieldInfo = runnerType.GetField("_runnerInst", BindingFlags.Static | BindingFlags.NonPublic);
+            fieldInfo.SetValue(null, fakeTestflowRunner);
+            fakeTestflowRunner.Initialize();
+            #endregion
+
+            _parameterChecker = fakeTestflowRunner.ParameterChecker;
+            _sequenceManager = fakeTestflowRunner.SequenceManager;
+            _interfaceManager = fakeTestflowRunner.ComInterfaceManager;
+
+            //IComInterfaceDescription description = _interfaceManager.GetComponentInterface(path + "JYUSB61902.dll");
+
+
+
+        }
+
+        [TestMethod]
+        public void CheckTestProject()
+        {
+            ISequenceGroup sequenceGroup = _sequenceManager.LoadSequenceGroup(Usr.SerializationTarget.File, testProjectPath);
+
+        }
+    }
+}
