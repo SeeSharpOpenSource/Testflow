582d4d53315de5ed5d6de4cc09f8baba277eb739
Stanley Wang <stanlegend@163.com>
2019-07-29 09:28:06 +0800

    添加了CallBackMessage所需的参数

--- source/src/Modules/Core/CoreCommon/Messages/CallBackMessage.cs
+++ source/src/Modules/Core/CoreCommon/Messages/CallBackMessage.cs
@@ -9,6 +9,8 @@ namespace Testflow.CoreCommon.Messages
     public class CallBackMessage : MessageBase
     {
         public List<string> Args { get; set; }
+        public bool SuccessFlag { get; set; }
+        public int CallBackId { get; set; }
 
         /// <summary>
         /// 创建回调返回消息实例
@@ -16,20 +18,25 @@ namespace Testflow.CoreCommon.Messages
         /// <param name="name">回调功能的名称</param>
         /// <param name="id">消息的ID</param>
         /// <param name="callBackArgs">回调参数</param>
-        public CallBackMessage(string name, int id, params string[] callBackArgs) : base(name, id, MessageType.Ctrl)
+        public CallBackMessage(string name, int id, int callBackId, params string[] callBackArgs) : base(name, id, MessageType.CallBack)
         {
             Args = new List<string>(callBackArgs);
+            this.CallBackId = callBackId;
         }
 
         public CallBackMessage(SerializationInfo info, StreamingContext context) : base(info, context)
         {
             this.Args = info.GetValue("Args", typeof (List<string>)) as List<string>;
+            this.SuccessFlag = (bool)info.GetValue("SuccessFlag", typeof(bool));
+            this.CallBackId = (int)info.GetValue("CallBackId", typeof(int));
         }
 
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
             base.GetObjectData(info, context);
             info.AddValue("Args", Args);
+            info.AddValue("SuccessFlag", SuccessFlag);
+            info.AddValue("CallBackId", CallBackId);
         }
     }
 }
