f7e2720d1a0a238631b0d9f1311a935807d9ea16
Stanley Wang <stanlegend@163.com>
2019-08-01 14:37:08 +0800

    添加了IResultManager接口的方法，准备初步测试

--- source/src/Dev/Common/Modules/IResultManager.cs
+++ source/src/Dev/Common/Modules/IResultManager.cs
@@ -1,10 +1,19 @@
-﻿namespace Testflow.Modules
+﻿using Testflow.Data;
+
+namespace Testflow.Modules
 {
     /// <summary>
     /// 结果管理模块
     /// </summary>
     public interface IResultManager : IController
     {
-         // TODO 需要根据业务进行详尽定义
+        /// <summary>
+        /// 根据用户所指定文件类型输出报告
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <param name="runtimeHash">运行时哈希值</param>
+        /// <param name="reportType">报告类型枚举，有txt文件，xml文件，json文件，和自定义报告</param>
+        /// <param name="customPrinter">可选参数，用于当reportType为custom的时候</param>
+        void PrintReport(string filePath, string runtimeHash, ReportType reportType, IResultPrinter customPrinter = null);
     }
 }
