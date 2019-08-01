64c276685f204437289962739a2f27de4b0609a0
Stanley Wang <stanlegend@163.com>
2019-08-01 15:37:34 +0800

    新添加了接口IResultPrinter和枚举ReportType

---
+++ source/src/Dev/Common/Data/IResultPrinter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testflow.Data
+{
+    //打印类接口用于给用户自定义打印类与方法
+    //默认有三种输出方法：txt,xml,json
+    //目前只实现.txt
+    public interface IResultPrinter
+    {
+        /// <summary>
+        /// 报表生成
+        /// </summary>
+        /// <param name="filePath">文件路径</param>
+        /// <param name="runtimeHash">运行时哈希</param>
+        void PrintReport(string filePath, string runtimeHash);
+    }
+}
---
+++ source/src/Dev/Common/Data/ReportType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testflow.Data
+{
+    public enum ReportType
+    {
+        txt = 1,
+        xml = 2,
+        json = 3,
+        custom = 4
+    }
+}
