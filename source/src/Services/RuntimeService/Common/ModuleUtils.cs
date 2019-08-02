e8332aa0a7dfe9105ba336df07e589af84d41094
Stanley Wang <stanlegend@163.com>
2019-08-02 17:37:41 +0800

    engineStart还是不返回异常了，异常由event来做

--- source/src/Services/RuntimeService/Common/ModuleUtils.cs
+++ source/src/Services/RuntimeService/Common/ModuleUtils.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Testflow.Data;
 using Testflow.Data.Sequence;
 using Testflow.Usr;
 
@@ -11,8 +12,6 @@ namespace Testflow.RuntimeService
 {
     internal static class ModuleUtils
     {
-        private static TestflowException Exception = null;
-
         internal static int GetSessionId(ITestProject testProject, ISequenceGroup sequenceGroup)
         {
             if (null == testProject)
@@ -51,30 +50,33 @@ namespace Testflow.RuntimeService
         //    return testProject.SequenceGroups.ElementAt(sessionID);
         //}
 
-        //在新的thread里运行engine
-        //返回抓到的错误信息
-        //如果没有错误，返回null
-        //这个可能不需要返回，因为外部用try, catch就能抓到这个错误？
-        internal static Exception EngineStartThread(ISequenceFlowContainer sequence)
+        
+        internal static void EngineStartThread(ISequenceFlowContainer sequence)
         {
             Thread engineThread = new Thread(() => ModuleUtils.EngineStart(sequence))
             {
                 IsBackground = true
             };
             engineThread.Start();
-            return Exception;
         }
 
+        //在这里为什么不做个变量去保存引擎的运行情况？
+        //因为事件触发:如果引擎失败，用户会根据相应的情况去添加事件
+        //在此就无需自己在这里返回exception
+        //而且还有一点。。无法返回exception给主线程。。除非加个锁？
         internal static void EngineStart(ISequenceFlowContainer sequence)
         {
             try
             {
-                TestflowRunner.GetInstance().EngineController.SetSequenceData(sequence);
                 TestflowRunner.GetInstance().EngineController.Start();
             }
             catch (TestflowException ex)
             {
-                Exception = ex;
+                //do nothing 只是调试时用的？
+            }
+            catch (Exception ex)
+            {
+                //do nothing 只是调试时用的？
             }
         }
     }
