5cc37f9f161c4a3b08c151d37da8036e644b438c
Stanley Wang <stanlegend@163.com>
2019-08-07 16:24:54 +0800

    添加新的CallBackType用以判断异步或是同步

---
+++ source/src/Dev/Common/Data/CallBackType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testflow.Data
+{
+    /// <summary>
+    /// 回调函数同步或者异步类型
+    /// </summary>
+    public enum CallBackType
+    {
+        /// <summary>
+        /// 同步
+        /// </summary>
+        Synchronous = 1,
+
+        /// <summary>
+        /// 异步
+        /// </summary>
+        Asynchronous = 2
+    }
+}
--- source/src/Dev/Common/Data/ReportType.cs
+++ source/src/Dev/Common/Data/ReportType.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Testflow.Data
+﻿namespace Testflow.Data
 {
+    /// <summary>
+    /// 报表类型生成
+    /// </summary>
     public enum ReportType
     {
+        /// <summary>
+        /// .txt格式
+        /// </summary>
         txt = 1,
+
+        /// <summary>
+        /// .xml格式
+        /// </summary>
         xml = 2,
+
+        /// <summary>
+        /// .json格式
+        /// </summary>
         json = 3,
+
+        /// <summary>
+        /// 自定义格式
+        /// </summary>
         custom = 4
     }
 }
