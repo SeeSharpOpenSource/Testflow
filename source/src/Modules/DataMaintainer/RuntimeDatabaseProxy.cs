d166f732878ade16f842e292b6c1b185ef1ba431
tao <tao.jing@jytek.com>
2019-09-18 14:27:06 +0800

    运行时DataMaintain新增对获取当前RuntimeHash相关信息的支持

--- source/src/Modules/DataMaintainer/RuntimeDatabaseProxy.cs
+++ source/src/Modules/DataMaintainer/RuntimeDatabaseProxy.cs
@@ -15,57 +15,12 @@ namespace Testflow.DataMaintainer
             throw new System.InvalidOperationException();
         }
 
-        public override TestInstanceData GetTestInstance(string runtimeHash)
-        {
-            throw new System.InvalidOperationException();
-        }
-
         public override IList<TestInstanceData> GetTestInstances(string filterString)
         {
             throw new System.InvalidOperationException();
         }
         
         public override void DeleteTestInstance(string fileterString)
-        {
-            throw new System.InvalidOperationException();
-        }
-
-        public override IList<SessionResultData> GetSessionResults(string runtimeHash)
-        {
-            throw new System.InvalidOperationException();
-        }
-
-        public override SessionResultData GetSessionResult(string runtimeHash, int sessionId)
-        {
-            throw new System.InvalidOperationException();
-        }
-
-        public override IList<SequenceResultData> GetSequenceResults(string runtimeHash, int sessionId)
-        {
-            throw new System.InvalidOperationException();
-        }
-
-        public override SequenceResultData GetSequenceResult(string runtimeHash, int sessionId, int sequenceIndex)
-        {
-            throw new System.InvalidOperationException();
-        }
-
-        public override IList<PerformanceStatus> GetPerformanceStatus(string runtimeHash, int session)
-        {
-            throw new System.InvalidOperationException();
-        }
-
-        public override PerformanceStatus GetPerformanceStatusByIndex(string runtimeHash, long index)
-        {
-            throw new System.InvalidOperationException();
-        }
-
-        public override IList<RuntimeStatusData> GetRuntimeStatus(string runtimeHash, int session)
-        {
-            throw new System.InvalidOperationException();
-        }
-
-        public override RuntimeStatusData GetRuntimeStatusByIndex(string runtimeHash, long index)
         {
             throw new System.InvalidOperationException();
         }
