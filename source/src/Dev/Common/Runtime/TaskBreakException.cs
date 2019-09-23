f6d12d4a21397504e391f05bf906bd0d1922a002
tao <tao.jing@jytek.com>
2019-09-23 16:30:50 +0800

    新增TaskBreakException

---
+++ source/src/Dev/Common/Runtime/TaskBreakException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Testflow.Runtime
+{
+    /// <summary>
+    /// 任务停止异常。在运行中途抛出该异常将停止序列的执行
+    /// </summary>
+    public class TaskBreakException : ApplicationException
+    {
+        /// <summary>
+        /// 创建TaskBreakException的实例
+        /// </summary>
+        public TaskBreakException() : base("Task running break.")
+        { }
+    }
+}
