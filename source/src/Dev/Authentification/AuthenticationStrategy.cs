34874b778281706a2653af4b152ed529a4faa0ea
tao <tao.jing@jytek.com>
2019-09-24 10:36:45 +0800

    新增Authentication模块

---
+++ source/src/Dev/Authentification/AuthenticationStrategy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestStation.Authentication
+{
+    public class AuthenticationStrategy
+    {
+        private readonly Dictionary<string, Dictionary<string, object>> _authorityData;
+        internal AuthenticationStrategy()
+        {
+            this._authorityData = new Dictionary<string, Dictionary<string, object>>(5);
+        }
+
+        public void AddUserLevel(string userLevel)
+        {
+
+        }
+
+        public void AddUserAuthority(string userLevel, string authorityName, bool value = true)
+        {
+
+        }
+
+        public void AddUserAuthority(string userLevel, string authorityName, object value)
+        {
+
+        }
+
+        public TDataType GetAuthorityInfo<TDataType>(string userLevel, string authorityName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
