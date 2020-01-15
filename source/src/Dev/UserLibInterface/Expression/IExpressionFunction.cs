29062f65f47daec6d7f160652cb78c1ccb0abd29
tao <tao.jing@jytek.com>
2020-01-15 15:07:21 +0800

    定义IExpressionFunction接口

---
+++ source/src/Dev/UserLibInterface/Expression/IExpressionFunction.cs
@@ -0,0 +1,31 @@
+﻿namespace Testflow.Usr.Expression
+{
+    /// <summary>
+    /// 表达式函数接口
+    /// </summary>
+    public interface IExpressionFunction
+    {
+        /// <summary>
+        /// 获取表达式的显示方式
+        /// </summary>
+        /// <param name="source">源数据</param>
+        /// <param name="target">目标数据</param>
+        /// <returns></returns>
+        string GetExpressionString(string source, string target);
+
+        /// <summary>
+        /// 判断source的值是否合法
+        /// </summary>
+        bool IsValidSource(object sourceValue);
+
+        /// <summary>
+        /// 判断target的值是否合法
+        /// </summary>
+        bool IsValidTarget(object targetValue);
+
+        /// <summary>
+        /// 计算表达式结果
+        /// </summary>
+        object Calculate(object sourceValue, object targetValue);
+    }
+}
