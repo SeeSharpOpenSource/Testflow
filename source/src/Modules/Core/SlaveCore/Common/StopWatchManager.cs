582e4480f220d3ebcd7550c32a86a154f98490a9
tao <tao.jing@jytek.com>
2020-04-14 11:52:56 +0800

    修复StopWatch计时错误的bug

--- source/src/Modules/Core/SlaveCore/Common/StopWatchManager.cs
+++ source/src/Modules/Core/SlaveCore/Common/StopWatchManager.cs
@@ -8,11 +8,13 @@ namespace Testflow.SlaveCore.Common
     {
         private readonly SlaveContext _context;
         private readonly Dictionary<int, Stopwatch> _stopWatches;
+        private long _frequency;
 
         public StopWatchManager(SlaveContext context)
         {
             this._context = context;
             this._stopWatches = new Dictionary<int, Stopwatch>(10);
+            this._frequency = Stopwatch.Frequency;
         }
 
         public void RegisterStopWatch(int coroutineId)
@@ -37,7 +39,8 @@ namespace Testflow.SlaveCore.Common
             //因为是线程间Diction的只读操作或者同一个StopWatch的线程内操作，无需做竞争处理
             Stopwatch stopWatch = _stopWatches[coroutineId];
             stopWatch.Stop();
-            return stopWatch.ElapsedTicks;
+            // 计算精确到微秒级别的数据
+            return (long)((double) stopWatch.ElapsedTicks/_frequency*1E6);
         }
 
         public void Dispose()
