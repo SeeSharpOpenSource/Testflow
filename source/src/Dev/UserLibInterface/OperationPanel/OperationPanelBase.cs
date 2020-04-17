4e1594a3e95d78098b10e6182e95b3429396d10e
tao <tao.jing@jytek.com>
2020-04-17 14:17:01 +0800

    为OperationPanelBase新增doc说明

--- source/src/Dev/UserLibInterface/OperationPanel/OperationPanelBase.cs
+++ source/src/Dev/UserLibInterface/OperationPanel/OperationPanelBase.cs
@@ -11,7 +11,7 @@ using Testflow.Utility.I18nUtil;
 
 namespace Testflow.ExtensionBase.OperationPanel
 {
-    public abstract class OperationPanelBase
+    public abstract class OperationPanelBase : IDisposable
     {
         public ISequenceFlowContainer SequenceData { get; private set; }
 
@@ -27,6 +27,10 @@ namespace Testflow.ExtensionBase.OperationPanel
             I18N.InitInstance(i18NOption);
         }
 
+        /// <summary>
+        /// 初始化OperationPanel
+        /// </summary>
+        /// <param name="sequenceData"></param>
         public virtual void Initialize(ISequenceFlowContainer sequenceData)
         {
             if (sequenceData is ITestProject)
@@ -45,16 +49,79 @@ namespace Testflow.ExtensionBase.OperationPanel
             this.SequenceData = sequenceData;
         }
 
+        /// <summary>
+        /// 测试生成开始回调方法
+        /// </summary>
+        /// <param name="generationInfo">生成状态信息</param>
         public abstract void TestGenerationStart(ITestGenerationInfo generationInfo);
+
+        /// <summary>
+        /// 测试生成结束回调方法
+        /// </summary>
+        /// <param name="generationInfo">生成结果信息</param>
         public abstract void TestGenerationOver(ITestGenerationInfo generationInfo);
+        
+        /// <summary>
+        /// 测试生成失败回调方法
+        /// </summary>
+        /// <param name="generationInfo">测试生成状态信息</param>
         public abstract void TestGenerationFailed(ITestGenerationInfo generationInfo);
+
+        /// <summary>
+        /// 测试生成开始回调方法
+        /// </summary>
+        /// <param name="startTime">测试开始执行时间</param>
         public abstract void TestStart(DateTime startTime);
+
+        /// <summary>
+        /// 会话开始执行回调方法
+        /// </summary>
+        /// <param name="session">会话的ID号</param>
+        /// <param name="time">会话开始的时间</param>
         public abstract void SessionStart(int session, DateTime time);
+
+        /// <summary>
+        /// 序列开始执行回调方法
+        /// </summary>
+        /// <param name="sequence">开始执行的序列</param>
+        /// <param name="time">开始执行的时间</param>
         public abstract void SequenceStart(ISequence sequence, DateTime time);
+
+        /// <summary>
+        /// 运行中获取状态回调方法
+        /// </summary>
+        /// <param name="currentStep">当前执行的Step</param>
+        /// <param name="runtimeInfo">运行时状态信息，可以获取当前会话的序列运行信息</param>
         public abstract void StatusReceived(ISequenceStep currentStep, IRuntimeStatusInfo runtimeInfo);
+
+        /// <summary>
+        /// 序列执行结束回调方法
+        /// </summary>
+        /// <param name="sequenceResult">序列执行的结果</param>
         public abstract void SequenceOver(ISequenceTestResult sequenceResult);
+
+        /// <summary>
+        /// 会话结束回调方法
+        /// </summary>
+        /// <param name="sessionResult">会话执行的结果</param>
         public abstract void SessionOver(ITestResultCollection sessionResult);
+
+        /// <summary>
+        /// 测试执行结束回调方法
+        /// </summary>
+        /// <param name="testResults">所有会话执行的结果列表</param>
         public abstract void TestOver(IList<ITestResultCollection> testResults);
+
+        /// <summary>
+        /// 发生异常时回调方法
+        /// </summary>
+        /// <param name="errorStep">出现错误的步骤</param>
+        /// <param name="failedInfo">错误的详细信息</param>
         public abstract void ErrorOccured(ISequenceStep errorStep, IFailedInfo failedInfo);
+
+        /// <summary>
+        /// 释放OperationPanel资源
+        /// </summary>
+        public abstract void Dispose();
     }
 }
