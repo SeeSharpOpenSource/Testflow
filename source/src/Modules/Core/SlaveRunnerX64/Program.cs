3fcc4159ce3ee5f757d238c399d94bd390cfbe3f
tao <tao.jing@jytek.com>
2020-06-28 10:24:14 +0800

    提交X86和X64 SlaveRunner的调用代码

--- source/src/Modules/Core/SlaveRunnerX64/Program.cs
+++ source/src/Modules/Core/SlaveRunnerX64/Program.cs
@@ -1,9 +1,14 @@
-﻿using Testflow.SlaveCore;
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using Testflow.SlaveCore;
 
 namespace Testflow.SlaveRunnerX64
 {
     class Program
     {
+        [HandleProcessCorruptedStateExceptions]
         static void Main(string[] args)
         {
             ProcessTestLauncher testLauncher = null;
@@ -12,10 +17,32 @@ namespace Testflow.SlaveRunnerX64
                 testLauncher = new ProcessTestLauncher(args[0]);
                 testLauncher.Start();
             }
+            catch (Exception ex)
+            {
+//                EventLog eventLog = new EventLog("TestFlowSlave");
+//                eventLog.Source = "TestFlowSlaveRunner";
+//                eventLog.WriteEntry(GetExceptionInfo(ex), EventLogEntryType.Error);
+            }
             finally
             {
                 testLauncher?.Dispose();
             }
+        }
+
+        private static string GetExceptionInfo(Exception ex)
+        {
+            StringBuilder errorInfo = new StringBuilder(2000);
+            errorInfo.Append(ex.Message)
+                .Append(Environment.NewLine)
+                .Append("ErrorCode:")
+                .Append(ex.HResult)
+                .Append(Environment.NewLine)
+                .Append(ex.StackTrace);
+            if (null != ex.InnerException)
+            {
+                errorInfo.Append(Environment.NewLine).Append(GetExceptionInfo(ex.InnerException));
+            }
+            return errorInfo.ToString();
         }
     }
 }
