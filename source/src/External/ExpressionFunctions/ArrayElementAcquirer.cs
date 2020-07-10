e81705e814ecb067a874747eb1e63cf80e5b754b
tao <tao.jing@jytek.com>
2020-07-10 11:47:47 +0800

    新增Array一维数组取下标的算法

---
+++ source/src/External/ExpressionFunctions/ArrayElementAcquirer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using Testflow.Data.Expression;
+using Testflow.Usr;
+
+namespace Testflow.External.ExpressionCalculators
+{
+    public class ArrayElementAcquirer : IExpressionCalculator
+    {
+        public ArrayElementAcquirer()
+        {
+            this.Operator = "GetElement";
+        }
+
+        public string Operator { get; }
+        public bool IsCalculable(object sourceValue, params object[] arguments)
+        {
+            return sourceValue is Array && ((Array)sourceValue).Rank == 1;
+        }
+
+        public object Calculate(object sourceValue, params object[] arguments)
+        {
+            return ((Array) sourceValue).GetValue((int)arguments[0]);
+        }
+    }
+}
